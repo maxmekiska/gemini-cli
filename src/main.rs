ed86d23b3f715e009b62c53956bb5f40015eea18
maxmekiska <maxmekiska@gmail.com>
2024-01-19 01:47:03 +0000

    feat: implemented cli commands/sub-commands infrastructure

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,53 @@
         3. undo: clears the last user and agent prompts 
 
 */
+use clap::{Parser, Subcommand};
+
 mod gemini;
 mod cliutils;
 mod chatroutine;
 
 
 
+#[derive(Parser)]
+#[command(name = "gemini-cli")]
+#[command(author = "Max Mekiska. <maxmekiska@gmail.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "CLI to interact with Googles gemini-pro LLM.", long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+
+#[derive(Subcommand)]
+enum Commands {
+    Chat {
+        #[arg(short, long, default_value_t = 0.7, help = "Model temperature.")]
+        temperature: f64,
+
+        #[arg(short, long, default_value_t = 800, help = "Maximum tokens the model should generate.")]
+        max_output_tokens: i32,
+
+        #[arg(short, long, default_value_t = 0.8, help = "topP.")]
+        p_top: f64,
+
+        #[arg(short, long, default_value_t = 10, help = "topK.")]
+        k_top: i32,
+    },
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    chatroutine::run_chat(0.7, 800, 0.8, 10).await?;
 
-    Ok(())
+    let args = Args::parse();
+
+    match &args.command {
+
+        Some(Commands::Chat { temperature, max_output_tokens, p_top, k_top }) => {
+            chatroutine::run_chat(*temperature, *max_output_tokens, *p_top, *k_top).await
+        }
+        None => Ok({})
+    }
 }
